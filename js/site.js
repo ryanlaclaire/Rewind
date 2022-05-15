062106debcdb99a130bb5e2dd6a466abbfb1f887
Ryan LaClaire <43187889+ryanlaclaire@users.noreply.github.com>
2022-05-15 18:56:45 -0400

    Initial Commit

--- js/site.js
+++ js/site.js
@@ -1,4 +1,35 @@
-//Call Hello World
-function helloWorld() {
-    alert("Hello World");
+//Get user input
+//Controller Function
+function getString() {
+
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("revString").value;
+    
+    let revString = reverseString(userString);
+    
+    displayString(revString);
+}
+
+//Reverse text in array
+//Logic Function
+function reverseString(userString) {
+
+    let revString = [];
+
+    for (let i = userString.length - 1; i >= 0; i--) {
+        revString += userString[i];
+    }
+
+    return revString;
+}
+
+//Display text on page
+//Display Function
+function displayString(revString) {
+
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+
+    document.getElementById("alert").classList.remove("invisible");
+
 }
